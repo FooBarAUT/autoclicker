6c87c1c804e9b2d0b30f90f2850b4de3af0edea9
FooBarAUT <39121688+FooBarAUT@users.noreply.github.com>
2023-07-09 10:43:15 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+import mouse
+import time
+
+width = 400
+height = 150
+loopCond = True
+
+
+# Dropdown menu options
+options = [
+    "right",
+    "middle",
+    "left"
+]
+
+
+def printInput():
+    inputNumber = entryInt.get()
+    inputMouse = chosenOption.get()
+    label.config(text="Input number was: " + str(inputNumber) + " and mouse " + str(inputMouse))
+
+        
+def click():
+    inputmillis = entryInt.get()
+    mousebutton = chosenOption.get()
+    if loopCond:
+        mouse.click(button=mousebutton)
+    root.after(inputmillis, click)
+
+        
+def start():
+    global loopCond
+    loopCond = True
+    root.after(1000, click)
+    
+def stop():
+    global loopCond
+    loopCond = False
+    
+# draw the window and give it a min size
+root = tk.Tk()
+root.geometry("400x150")
+root.minsize(width, height)
+entryInt = tk.IntVar()
+
+# draw the input field and place it
+timer = tk.Label(root, text = "Time in ms").place(x = 30, y = 50)
+entry = tk.Entry(root, textvariable=entryInt).place(x = 100, y = 50)
+
+# draw the dropdown and place it
+chosenOption = tk.StringVar()
+chosenOption.set("left")
+drop = tk.OptionMenu(root, chosenOption, *options)
+drop.place(x=250, y=42)
+
+# draw the buttons and place them
+startbtn = tk.Button(root, text = "START", command = start, activebackground = "green", activeforeground = "blue").place(x = width/2 - 65, y = height-50)
+stopbtn = tk.Button(root, text = "STOP", command = stop, activebackground = "green", activeforeground = "blue").place(x = width/2 + 55, y = height-50)
+
+# debugging to see input and chosen option, will be deleted later
+label = tk.Label(root, text = "")
+label.pack()
+
+#start the main loop-di-loop
+root.attributes('-topmost',True)
+root.mainloop()
